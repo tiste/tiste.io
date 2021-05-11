a48a04d641978de531e97deb7e176fc39231c8b3
tiste <tiste@users.noreply.github.com>
2021-05-11 15:21:01 +0000

    deploy: 04294340a14724f400883f11ba6a7fed29dd48d4

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-eee937842ddff0bb6cbc.js"
+    "url": "webpack-runtime-a589b43ec32e40988565.js"
   },
   {
     "url": "framework-555e15aa87d6c74cd891.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "7dbb0517aa22145b1ece6f77faed845f"
+    "revision": "12fe248e556e818f41e001501b4ace58"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-dac8329f6865b0887d7a.js"
